4d2272cbdfb8384f890eb53689f3757a54810654
Oliver Gressel <gressel@nbi.ku.dk>
2021-07-14 13:43:56 +0200

    extend temperature displays.

--- generate.sh
+++ generate.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
 #
-# ---=== TorquePro dashboard generator for Bolt EV / Opel Ampera-e
+# ---=== TorquePro dashboard generator for Bolt EV / Ampera-e ===---
 #
 
-nid=0
+# --- screen dimensions
 
 xdisp=1080
 ydisp=1920
 
-# --- various temperature sensors
+# ---  for generating unique IDs
 
-x0=$((12-xdisp))
-y0=72
+nid=0
+
+# ---=== VARIOUS TEMPERATURE SENSORS ===---
+
+x0=$((24-xdisp))
+y0=96
 
 cid=0
 
-for id in $(seq 0 5); do
+pids=( 326     2261022 2261023
+       2244823 2244824 2244825
+       2244826 2244827 2244828
+       2245455 2245028 )
+
+lbls=( "Air Temp 0" "Air Temp 1" "Air Temp 2"
+       "Batt Sec 1" "Batt Sec 2" "Batt Sec 3"
+       "Batt Sec 4" "Batt Sec 5" "Batt Sec 6"
+       "Batt Temp"  "Coolant Temp" )
+
+upids=( "\\\\!Air Temp 0" "*Air Temp 1" "*Air Temp 2"
+        "*BECM Battery Section 1 Temp" "*BECM Battery Section 2 Temp"
+        "*BECM Battery Section 3 Temp" "*BECM Battery Section 4 Temp"
+        "*BECM Battery Section 5 Temp" "*BECM Battery Section 6 Temp"
+        "\\\\!Batt Temp" "*BECM Battery Coolant Temp ?" )
+
+for id in $(seq 0 10); do
 
     cid=$((cid+1))
 
-    num=$(printf "%01d" $((id+1)))
-    pid=$((id+2244823))
+    # --- lookup labels
+
+    pid=${pids[$id]}
+    lbl=${lbls[$id]}
+    upid=${upids[$id]}
 
     # --- screen coordinates
 
-    xcoord=$(( x0 + 216*$(( id%2 )) ))
-    ycoord=$(( y0 + 216*$(( id/2 )) ))
+    xcoord=$(( x0 + 224*$(( id%3 )) ))
+    ycoord=$(( y0 + 224*$(( id/3 )) ))
 
     # --- write .dash file
 
-    cat template-sect-temperature.dash |\
+    cat template-temperature.dash |\
         sed "s/{ID}/$((id+nid))/g" |\
-        sed "s/{NUM}/$num/g" |\
+        sed "s/{LBL}/$lbl/g" |\
+        sed "s/{USRPID}/$upid/g" |\
         sed "s/{PID}/$pid/g" |\
         sed "s/{XCOORD}/$xcoord/g" |\
         sed "s/{YCOORD}/$ycoord/g"
@@ -40,7 +64,7 @@ done
 
 nid=$((nid+cid))
 
-# --- battery cell voltage indicators
+# ---=== BATTERY CELL VOLTAGE INDICATORS ===---
 
 x0=3
 y0=72
